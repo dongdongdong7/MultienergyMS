231f745b8e8cfea6d83c3c55cd060e9bd3c9fadb
spd <384220729@qq.com>
2024-05-27 14:16:59 +0800

    Update CA, plotMultiMS,cal_cor.

--- R/function.R
+++ R/function.R
@@ -14,6 +14,51 @@ create_demo <- function(){
                  correlation = c(-0.8, -0.4, 0.5, -0.8, -0.2, 0.8, 0.2, -0.5, -0.8))
   return(demo)
 }
+#' @title cal_cor
+#' @description
+#' calculate correlation df.
+#'
+#' @param data A tibble.
+#' @param ceLength ceLength.
+#' @param noise noise.
+#'
+#' @return A tibble.
+#' @export
+#'
+#' @examples
+#' data(CA)
+#' df <- cal_cor(CA, max_CE = 35)
+cal_cor <- function(data, ceLength = 10, noise = 200, min_CE = NA, max_CE = NA){
+  if(is.na(min_CE)) min_CE <- min(data$CE)
+  if(is.na(max_CE)) max_CE <- max(data$CE)
+  if(min_CE >= max_CE) stop("CE wrong!")
+  ceIdx <- seq(min_CE, max_CE, ceLength)
+  dfList <- lapply(1:(length(ceIdx) - 1), function(i) {
+    ceRange <- c(ceIdx[i], ceIdx[i + 1])
+    data_tmp <- data %>%
+      dplyr::filter(CE >= ceRange[1] & CE <= ceRange[2])
+    data_tmp[, -1][data_tmp[, -1] <= noise] <- NA
+    corMatrix <- round(cor(data_tmp[, -1], use = "pairwise.complete.obs"), 2)
+    mz <- colnames(corMatrix)
+    df <- dplyr::tibble(mz1 = double(), mz2 = double(), correlation = double())
+    for(mz1 in mz){
+      for(mz2 in mz){
+        correlation_tmp <- corMatrix[mz1, mz2]
+        df_tmp <- dplyr::tibble(mz1 = as.double(mz1),
+                                mz2 = as.double(mz2),
+                                correlation = correlation_tmp)
+        df <- rbind(df, df_tmp)
+      }
+    }
+    df$range <- i
+    return(df)
+  })
+  df <- purrr::list_rbind(dfList)
+  df$correlation[is.na(df$correlation)] <- 0
+  df <- df %>%
+    dplyr::arrange(mz1)
+  return(df)
+}
 #' @title paraSet
 #' @description
 #' Set parameters for df
@@ -26,17 +71,11 @@ create_demo <- function(){
 #' @export
 #'
 #' @examples
-#' demo <- paraSet(df = demo)
+#' df <- paraSet(df = df)
 paraSet <- function(df, corThreshold = 0.8){
   df$min_mz <- min(c(df$mz1, df$mz2))
   df$max_mz <- max(c(df$mz1, df$mz2))
-  df$size <- sapply(df$range, function(x) {
-    switch (x,
-            "1" = 4,
-            "2" = 6,
-            "3" = 12
-    )
-  })
+  df$size <- df$range^3
   df$color <- sapply(df$correlation, function(x) {
     if(abs(x) < corThreshold) return(0)
     else return(x)
@@ -54,13 +93,13 @@ paraSet <- function(df, corThreshold = 0.8){
 #' @export
 #'
 #' @examples
-#' plotMultiMS(df = demo)
-plotMultiMS <- function(df){
+#' plotMultiMS(df = df, scale_size = c(2, 10))
+plotMultiMS <- function(df, scale_size = c(4, 16), stroke = 1){
   min_mz <- df$min_mz[1]
   max_mz <- df$max_mz[1]
   p <- ggplot2::ggplot(df, ggplot2::aes(x = mz1, y = mz2)) +
-    ggplot2::geom_point(ggplot2::aes(size = size, col = color), stroke = 2, shape = 21, alpha = 0.9) +
-    ggplot2::scale_size(range = c(4, 16)) +
+    ggplot2::geom_point(ggplot2::aes(size = size, col = color), stroke = stroke, shape = 21, alpha = 0.9) +
+    ggplot2::scale_size(range = scale_size) +
     ggplot2::scale_color_gradient2(low = "#2600D1FF", high = "#D60C00FF", mid = "white", midpoint = 0) +
     ggplot2::theme_grey() +
     ggplot2::theme(legend.position = "none") +
--- test.R
+++ test.R
@@ -1,64 +1,8 @@
 devtools::document()
-library(magrittr)
-load("./test_data/CA.RData")
-CA <- readr::read_csv("./test_data/CA.csv")
-CA
-ceRange1 <- c(5, 15)
-ceRange2 <- c(15, 25)
-ceRange3 <- c(25, 35)
-rThreshold <- 0.2
-noise <- 200
-data1 <- CA %>%
-  dplyr::filter(CE >= ceRange1[1] & CE <= ceRange1[2])
-data1[, -1][data1[, -1] <= noise] <- NA
-corMatrix1 <- round(cor(data1[, -1], use = "pairwise.complete.obs"), 2)
-mz <- colnames(corMatrix1)
-df1 <- dplyr::tibble(mz1 = double(), mz2 = double(), correlation = double())
-for(mz1 in mz){
-  for(mz2 in mz){
-    correlation_tmp <- corMatrix1[mz1, mz2]
-    df_tmp <- dplyr::tibble(mz1 = as.double(mz1),
-                            mz2 = as.double(mz2),
-                            correlation = correlation_tmp)
-    df1 <- rbind(df1, df_tmp)
-  }
-}
-df1$range <- 1
+#load("./test_data/CA.RData")
+#CA <- readr::read_csv("./test_data/CA.csv")
 
-data2 <- CA %>%
-  dplyr::filter(CE >= ceRange2[1] & CE <= ceRange2[2])
-data2[, -1][data2[, -1] <= noise] <- NA
-corMatrix2 <- round(cor(data2[, -1], use = "pairwise.complete.obs"), 2)
-df2 <- dplyr::tibble(mz1 = double(), mz2 = double(), correlation = double())
-for(mz1 in mz){
-  for(mz2 in mz){
-    correlation_tmp <- corMatrix2[mz1, mz2]
-    df_tmp <- dplyr::tibble(mz1 = as.double(mz1),
-                            mz2 = as.double(mz2),
-                            correlation = correlation_tmp)
-    df2 <- rbind(df2, df_tmp)
-  }
-}
-df2$range <- 2
-
-data3 <- CA %>%
-  dplyr::filter(CE >= ceRange3[1] & CE <= ceRange3[2])
-data3[, -1][data3[, -1] <= noise] <- NA
-corMatrix3 <- round(cor(data3[, -1], use = "pairwise.complete.obs"), 2)
-df3 <- dplyr::tibble(mz1 = double(), mz2 = double(), correlation = double())
-for(mz1 in mz){
-  for(mz2 in mz){
-    correlation_tmp <- corMatrix3[mz1, mz2]
-    df_tmp <- dplyr::tibble(mz1 = as.double(mz1),
-                            mz2 = as.double(mz2),
-                            correlation = correlation_tmp)
-    df3 <- rbind(df3, df_tmp)
-  }
-}
-df3$range <- 3
-
-df <- rbind(df1, df2, df3) %>%
-  dplyr::arrange(mz1)
-
-demo <- paraSet(df = df, corThreshold = 0.6)
-plotMultiMS(df = demo)
+data(CA)
+df <- cal_cor(CA, max_CE = 45, ceLength = 10, noise = 1000)
+df <- paraSet(df, corThreshold = 0.8)
+plotMultiMS(df = df, scale_size = c(4, 10),stroke = 0.5)
