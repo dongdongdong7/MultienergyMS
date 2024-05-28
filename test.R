cebcc44260f30cb9f71520e99fcb324c432579cc
spd <384220729@qq.com>
2024-05-28 09:17:32 +0800

    Update DESCRIPTION

--- test.R
+++ test.R
@@ -3,6 +3,6 @@ devtools::document()
 #CA <- readr::read_csv("./test_data/CA.csv")
 
 data(CA)
-df <- cal_cor(CA, max_CE = 45, ceLength = 10, noise = 1000)
-df <- paraSet(df, corThreshold = 0.6)
+df <- cal_cor(CA, min_CE = 5, max_CE = 60, ceLength = 10, noise = 1000)
+df <- paraSet(df, corThreshold = 0.4)
 plotMultiMS(df = df, scale_size = c(2, 16),stroke = 1)
